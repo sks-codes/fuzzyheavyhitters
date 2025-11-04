1b8f876ab6e2f62f5a14760ac02dc24453e601b6
Ubuntu <ubuntu@ip-172-31-33-177.ec2.internal>
2025-11-04 19:58:09 +0000

    still coding unknown naive

--- src/naive/protocol.rs
+++ src/naive/protocol.rs
@@ -1,8 +1,13 @@
+use crossbeam::thread;
+
 use crate::{
     channel::CommTrackingChannel,
     fss::dpf::DpfKey,
-    fuzzy_match::share_phase::ShareConfig,
+    fuzzy_match::share_phase::ShareConfig, 
+    util::u128_to_bits_msb,
 };
+
+const BATCH_SIZE_PER_CORE: usize = 10;
 
 pub struct NaiveProtocol {
     share_config: ShareConfig,
@@ -44,15 +49,62 @@ impl NaiveProtocol {
     pub fn run_server_known_dictionary_parallel(
         &self,
         client_shares: &[DpfKey<1>],
-        query_ponts: &[Vec<u128>],
+        query_points: &[Vec<u128>],
+        thread_pool: &ThreadPool,
         other_server_channels: &mut [CommTrackingChannel],
     ) -> Result<Vec<bool>, anyhow::Error> {
-        
+        let num_threads = other_server_channels.len();
+
+        let modulus = 1u128 << self.share_config.h2;
+
+        let mut query_points_bits: Vec<Vec<bool>> = vec![vec![]; query_points.len()];
+        thread_pool.install(|| {
+            query_points_bits
+                .par_iter_mut()
+                .zip(query_points.par_iter())
+                .for_each(|(bits, point)| {
+                    let point_bits = point.iter()
+                        .map(|x| u128_to_bits_msb(x, self.share_config.h1))
+                        .collect::<Vec<Vec<bool>>>();
+                    for i in 0..self.share_config.h1 {
+                        for dimension in 0..self.share_config.d {
+                            bits.push(point_bits[dimension][i]);
+                        }
+                    }
+                });
+        });
+
+        let mut server_bits = vec![false; query_points_bits.len()];
+        server_bits
+            .chunks_mut(num_threads * BATCH_SIZE_PER_CORE)
+            .zip(query_points_bits.chunks(num_threads * BATCH_SIZE_PER_CORE))
+            .try_for_each(|(server_bits_chunk, query_points_bit_chunk)| {
+                let mut evals = Vec::new();
+                let chunk_size = (query_points_chunk.len() + num_threads - 1) / num_threads;
+                for query_point_bits in query_points_bits_chunk {
+                    let mut aggregated_eval = 0u128;
+                    thread_pool.install(|| {
+                        let point_evals = client_shares
+                            .par_iter()
+                            .map(|dpf_key| {
+                                dpf_key.eval_pdf(&query_point_bits, modulus)
+                            })
+                            .collect::<Vec<u128>>();
+                        aggregated_eval = _point_evals.iter()
+                            .fold(0u128, |acc, &x| (acc + x) % modulus);
+                    });
+                    evals.push(aggregated_eval);
+                }
+                // TODO batch check
+            }).map_err(|e| anyhow::anyhow!("Error during parallel processing: {}", e))?;
     }
 
     fn batch_check(
         &self,
-        current_points: &[Vec<u128>],
+        evals: &[u128],
         other_server_channels: &mut [CommTrackingChannel],
-    )
+    ) -> Result<Vec<bool>, anyhow::Error> {
+        // Implement batch checking logic here
+        Ok(vec![false; evals.len()]) // Placeholder
+    }
 }
